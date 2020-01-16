ac0491aaaaf8576bed94da4943a2822e28effa3a
Jacqueline Almeida <jackellita@hotmail.com>
2020-01-16 08:56:51 -0300

    Depot Scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+Product.delete_all
+
+Product.create!(title: 'Mobile',
+  description: %{Mobile Phone iPhone 6s},
+  image_url: 'https://t.tudocdn.net/143323?w=149&h=304',
+  price: 1000.00)
+
+Product.create!(title: 'Mobile Phone 2',
+  description: %{Mobile Phone iPhone 7s},
+  image_url: 'https://t.tudocdn.net/143323?w=149&h=304',
+  price: 1500.00)
+
+Product.create!(title: 'Mobile Phone 3',
+  description: %{Mobile Phone iPhone 8},
+  image_url: 'https://t.tudocdn.net/143323?w=149&h=304',
+  price: 1700.00)
